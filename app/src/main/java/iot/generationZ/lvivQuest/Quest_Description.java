43422bd33103ca923e3160b5260b4ce1e1bf78c8
julia bashko <julia0615.jb@gmali.com>
2017-11-16 18:54:05 +0200

    added new activity (quest description)

---
+++ app/src/main/java/iot/generationZ/lvivQuest/Quest_Description.java
@@ -0,0 +1,13 @@
+package iot.generationZ.lvivQuest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Quest_Description extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quest__description);
+    }
+}
